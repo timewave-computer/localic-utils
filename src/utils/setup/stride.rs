f4b0014008584c89ede63fdf14756091ad01f7dd
keyne <kleuyckx@gmail.com>
2024-07-10 12:15:24 +0100

    fix up set host zone

--- src/utils/setup/stride.rs
+++ src/utils/setup/stride.rs
@@ -58,7 +58,7 @@ impl TestContext {
     pub fn set_up_stride_host_zone(&mut self, dest_chain: &str) {
         let native_denom = self.get_native_denom().src(dest_chain).get().clone();
 
-        let host_denom_on_stride = self.get_ibc_denom(&native_denom, STRIDE_CHAIN_NAME, dest_chain);
+        let host_denom_on_stride = self.get_ibc_denom(&native_denom, dest_chain, STRIDE_CHAIN_NAME);
 
         let stride = self.get_chain(STRIDE_CHAIN_NAME);
         let stride_rb = &stride.rb;
